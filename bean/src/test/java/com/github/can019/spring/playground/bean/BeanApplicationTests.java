4b68bcd83ebdd1d2cd8099e943d0d6929285ac9c
can019 <jys01012@gmail.com>
2025-05-23 10:16:54 +0900

    chore: init bean

---
+++ bean/src/test/java/com/github/can019/spring/playground/bean/BeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.can019.spring.playground.bean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
