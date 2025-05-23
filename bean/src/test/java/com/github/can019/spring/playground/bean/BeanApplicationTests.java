8bd88154cff097ef6406d78ee4c3e53235bf1433
can019 <jys01012@gmail.com>
2025-05-23 10:44:50 +0900

    feat: Bean Scope 테스트 - singleton, prototype

--- bean/src/test/java/com/github/can019/spring/playground/bean/BeanApplicationTests.java
+++ bean/src/test/java/com/github/can019/spring/playground/bean/BeanApplicationTests.java
@@ -1,13 +1,33 @@
 package com.github.can019.spring.playground.bean;
 
+import com.github.can019.spring.playground.bean.scope.PrototypeScopeWrapper;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class BeanApplicationTests {
+	@Autowired
+	private ApplicationContext context;
 
 	@Test
-	void contextLoads() {
-	}
+	void prototypeBeanTest() {
+		PrototypeScopeWrapper prototypeScopeWrapper1 = context.getBean(PrototypeScopeWrapper.class);
+		PrototypeScopeWrapper prototypeScopeWrapper2 = context.getBean(PrototypeScopeWrapper.class);
 
+		System.out.println("1: " +prototypeScopeWrapper1);
+		System.out.println("2: "+ prototypeScopeWrapper2);
+
+
+
+		System.out.println("1: " +prototypeScopeWrapper1.inner);
+		System.out.println("2: "+ prototypeScopeWrapper2.inner);
+
+		System.out.println("1: " +prototypeScopeWrapper1.inner.nestedInner);
+		System.out.println("2: "+ prototypeScopeWrapper2.inner.nestedInner);
+
+		Assertions.assertThat(prototypeScopeWrapper1).isNotEqualTo(prototypeScopeWrapper2);
+	}
 }
---
+++ bean/src/test/java/com/github/can019/spring/playground/bean/scope/PrototypeScopeWrapperTest.java
@@ -0,0 +1,7 @@
+package com.github.can019.spring.playground.bean.scope;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrototypeScopeWrapperTest {
+
+}
